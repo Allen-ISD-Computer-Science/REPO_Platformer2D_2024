ad8938aa7f203b9c7fcd55abfa9aa25ec5d25f76
Faraz Piracha <faraz.piracha@student.allenisd.org>
2024-02-08 22:40:59 -0600

    Fixed jump and movement bug, also changed jump button to up and W instead of space, #44

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private LayerMask jumpableGround;
     private Rigidbody2D rbAva;
+    private BoxCollider2D coll;
+
 
     // Start is called before the first frame update
     private void Start()
     {
         rbAva = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -18,9 +22,14 @@ public class PlayerMovement : MonoBehaviour
         float dirX = Input.GetAxisRaw("Horizontal");
         rbAva.velocity = new Vector2(dirX * 7f, rbAva.velocity.y);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            rbAva.velocity = new Vector2(rbAva.velocity.x, 14f);
+            rbAva.velocity = new Vector2(rbAva.velocity.x, 9f);
         }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
     }
 }

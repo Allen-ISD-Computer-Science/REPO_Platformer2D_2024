a2b6dc485fea2aa2ccae9f7e6931ccaa1446d4e9
Shriya-Bansal <shriya.bansal@student.allenisd.org>
2024-02-09 06:53:10 -0600

    Parallax Background
    Created forest background and embedded additional details. One bug remaining in Parallax background.

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Parallax : MonoBehaviour
 {
     private float length, startpos;
     public GameObject cam;
@@ -16,10 +16,14 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        float temp = (cam.transform.position.x * (1 - parallaxEffect))
         float dist = (cam.transform.position.x * parallaxEffect);
 
         transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z );
+
+        if (temp > startpos + length) startpos += length
+        else if (temp < startpos - length) startpos -= length;
     }
 }

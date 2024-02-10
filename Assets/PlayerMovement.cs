d95e2260b0e2370aa2e19ba419766cd5c790b18f
SophieH8 <sophiesoccer125@gmail.com>
2024-02-09 21:28:04 -0600

    Made the sprite face different directions when walking
    Added lines to the code to make the sprite face left when walking left and right when facing right. Works.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,14 +6,17 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private LayerMask jumpableGround;
     private Rigidbody2D rbAva;
+    private SpriteRenderer sprite;
     private BoxCollider2D coll;
     private Animator anim;
+    private float dirX = 0f;
 
 
     // Start is called before the first frame update
     private void Start()
     {
         rbAva = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
         coll = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
     }
@@ -21,22 +24,27 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
         rbAva.velocity = new Vector2(dirX * 7f, rbAva.velocity.y);
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rbAva.velocity = new Vector2(rbAva.velocity.x, 9f);
             }
+        UpdateAnimationState();
+    }
 
-
+    private void UpdateAnimationState()
+    {
         if (dirX > 0f)
         {
             anim.SetBool("walking", true);
+            sprite.flipX = false;
         }
         else if (dirX < 0f)
         {
             anim.SetBool("walking", true);
+            sprite.flipX = true;
         }
         else
         {

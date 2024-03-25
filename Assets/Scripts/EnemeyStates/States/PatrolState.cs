691b7b5191317c67c3a4fc86e8483e75d5846b3e
EighthRisen <battonld@gmail.com>
2024-03-25 10:39:20 -0500

    Slimeball now can switch from states
    So I added that the enemy can detect when the player is around him. When the player is in the right distance he will stop moving and play an animation until the player leaves. Also organized the enemy scripts (now instead of PatrolScript.cs there is a main controller called enemy.cs, patrolState.cs, PlayerDetectedState.cs and EnemyBaseState.cs) so it is easier to add more states to the enemy.

---
+++ Assets/Scripts/EnemeyStates/States/PatrolState.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolState : EnemyBaseState
+{
+    public PatrolState(Enemy enemy, string animationName) : base (enemy, animationName)
+    {
+        
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (enemy.CheckForPlayer())
+            enemy.SwitchState(enemy.playerDetectedState);
+        if (enemy.CheckForObstacles())
+            Rotate();
+        
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+        
+            if (enemy.facingRight)
+                enemy.rb.velocity = new Vector2(enemy.speed, enemy.rb.velocity.y);
+            else
+                enemy.rb.velocity = new Vector2(-enemy.speed, enemy.rb.velocity.y);   
+    }
+    void Rotate()
+    {
+        enemy.transform.Rotate(0, 180, 0);
+        enemy.facingRight = !enemy.facingRight;
+    }
+}

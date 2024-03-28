2b4809e34c8db2b72653cde52b86997fa220bc62
Faraz Piracha <faraz.piracha@student.allenisd.org>
2024-03-27 22:45:48 -0500

    modified enemy scripts to allow for dropping items, made apple 3 drop after dummy is defeated, moved apple 3 -BasicEnemyHealth and AdvancedEnemyHealth now have the option to have an item drop after death -This was implemented on the dummy, making apple 3 its item -apple 3 was moved to the side -initally tried to have apple 3 appear where the dummy was, but this caused it to be instantly collected -untill sword collecting fruit bug is fixed apple will appear to the side
    Supports: #84

--- Assets/Scripts/AdvancedEnemyHealth.cs
+++ Assets/Scripts/AdvancedEnemyHealth.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class AdvancedEnemyHealth : MonoBehaviour
 {
     [SerializeField] private int healthPoints = 3;
+    [SerializeField] private bool dropItem = false;
+    [SerializeField] public GameObject droppedItem;
 
 
-//Advanced enimies will take multiple hits to kill so they have health points and will die after loosing all their health
-public void Damage(int amount) {
+
+    //Advanced enimies will take multiple hits to kill so they have health points and will die after loosing all their health
+    public void Damage(int amount)
+    {
         //Debug.Log("Ouch!");
         this.healthPoints -= amount;
 
-        if (healthPoints <= 0) {
+        if (healthPoints <= 0)
+        {
             Die();
-            }
+        }
     }
 
 
-private void Die() {
-    //Debug.Log("Enemy Dead");
-    Destroy(gameObject);
+    private void Die()
+    {
+        //Debug.Log("Enemy Dead");
+        Destroy(gameObject);
+
+        //after enemy death if dropItem is enabled set the dropped item to active so it "drops"
+        if (dropItem)
+        {
+            droppedItem.SetActive(true);
+        }
     }
 }
--- Assets/Scripts/BasicEnemyHealth.cs
+++ Assets/Scripts/BasicEnemyHealth.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class BasicEnemyHealth : MonoBehaviour
 {
 
-//basic enimies will die in one hit so they don't require any health points
-public void Damage() {
+    [SerializeField] private bool dropItem = false;
+    [SerializeField] public GameObject droppedItem;
+
+    //basic enimies will die in one hit so they don't require any health points
+    public void Damage()
+    {
         //Debug.Log("Ouch!");
         Die();
+    }
+
+
+    private void Die()
+    {
+        //Debug.Log("Enemy Dead");
+        Destroy(gameObject);
+
+        //after enemy death if dropItem is enabled set the dropped item to active so it "drops"
+        if (dropItem)
+        {
+            droppedItem.SetActive(true);
         }
 
-    
-private void Die() {
-    //Debug.Log("Enemy Dead");
-    Destroy(gameObject);
     }
 }
 
